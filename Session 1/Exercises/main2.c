e636f777e5fb206278e53f88508a126f2a2c4be5
Firman Subagja <subagja.firman@gmail.com>
2024-11-04 11:44:15 +0700

    add main2

---
+++ Session 1/Exercises/main2.c
@@ -0,0 +1,16 @@
+/*
+   Selection process
+
+   Definisi masalah
+      Membuat program untuk pengecekan
+      apakah bilangan bulat ini bilangan 
+      positif, bilangan negatif, atau
+      bilangan nol.
+   
+   Membuat model masalah
+      angka > 0? tampilkan bilangan positif
+      angka < 0? tampilkan bilangan negatif
+      angka == 0? tampilkan bilangan nol
+
+   
+*/
