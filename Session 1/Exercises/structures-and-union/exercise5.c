58835d682aa172eef8b22e456da69beea7264f14
Firman Subagja <subagja.firman@gmail.com>
2024-11-06 15:01:36 +0700

    adjust: exercise 5

--- Session 1/Exercises/structures-and-union/exercise5.c
+++ Session 1/Exercises/structures-and-union/exercise5.c
@@ -7,29 +7,45 @@ struct Employee {
    float salary;
 };
 
-void updateSalary(struct Employee *emp, float newSalary) {
-   emp->salary = newSalary;
+void updateSalary(struct Employee *emp, float newSalary, int index) {
+   emp[index].salary = newSalary;
 }
 
-void displayEmployee(struct Employee emp) {
-   printf("ID: %d\n", emp.id);
-   printf("Name: %s\n", emp.name);
-   printf("Salary: %.2f\n", emp.salary);
+void displayEmployee(struct Employee *emp, int size) {
+   int i = 0;
+   while (i < size) {
+      printf("ID: %d\n", emp[i].id);
+      printf("Name: %s\n", emp[i].name);
+      printf("Salary: %.2f\n", emp[i].salary);
+      i++;
+   }
 }
 
 int main() {
-   struct Employee emp1;
-   emp1.id = 101;
-   strcpy(emp1.name, "Budi");
-   emp1.salary = 5000.0;
+   int size = 3;
+   struct Employee emp1[size];
+
+   emp1[0].id = 1;
+   strcpy(emp1[0].name, "Firman");
+   emp1[0].salary = 4500.0;
+
+   emp1[1].id = 2;
+   strcpy(emp1[1].name, "Farhan");
+   emp1[1].salary = 6000.0;
+
+   emp1[2].id = 3;
+   strcpy(emp1[2].name, "Dalfan");
+   emp1[2].salary = 5500.0;
 
    printf("Data karyawan sebelum update:\n");
-   displayEmployee(emp1);
+   displayEmployee(emp1, size);
 
-   updateSalary(&emp1, 7500.0);
+   updateSalary(emp1, 7500.0, 0);
+   updateSalary(emp1, 8500.0, 1);
+   updateSalary(emp1, 9500.0, 2);
 
-   printf("Data karyawan sesudah update:\n");
-   displayEmployee(emp1);
+   printf("\nData karyawan sesudah update:\n");
+   displayEmployee(emp1, size);
    
    return 0;
 }
