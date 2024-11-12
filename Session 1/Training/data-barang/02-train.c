fc74d70cd3f674a98bd72c88a5f412aaa8c35686
firmansubagjaa <subagja.firman33@gmail.com>
2024-11-12 21:56:37 +0700

    adjust: add function addData

--- Session 1/Training/data-barang/02-train.c
+++ Session 1/Training/data-barang/02-train.c
@@ -8,12 +8,30 @@ struct Barang {
     float harga;
 };
 
-FILE *openFile(const char filename) {
+FILE *openFile(const char *filename) {
     FILE *check = fopen(filename, "r");
     FILE *file = fopen(filename, "w");
     if (file == NULL) {
         fprintf(stderr, "Terjadi kesalahan.");
         return NULL;
     }
-    
+    printf("\nBerhasil untuk %s file %s.\n", check == NULL ? "Membuat" : "Membuka");
+    return file;
+}
+
+void closeFile(FILE *file) {
+    if (file != NULL) {
+        fclose(file);
+        printf("\nBerhasil untuk menutupkan file.\n");
+    }
+}
+
+void addData(FILE *file, const char *filename) {
+    if (file == NULL) {
+        fprintf(stderr, "Terjadi kesalahan pada saat menambahkan data.");
+        return;
+    }
+    fprintf(file, "Nama: Firman");
+    fprintf(file, "NIM: A001");
+    fprintf(file, "Nilai: 100");
 }
